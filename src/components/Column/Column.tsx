fa1e63b47b488a34004b655689a7bf3728427e68
Filip Kajetaniak <kajetaniakkfilip@gmail.com>
2019-08-18 21:26:31 +0200

    Add column slider lines

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -3,5 +3,9 @@ import React, { FC } from "react";
 import "./Column.sass";
 
 export const Column: FC = () => {
-  return <div className="column" />;
+  return (
+    <div className="column">
+      <div className="chord-slider" />
+    </div>
+  );
 };
