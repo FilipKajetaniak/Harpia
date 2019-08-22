d1f15a7c0b49c254688c1eef4dd377f84f5b0c21
Filip Kajetaniak <kajetaniakkfilip@gmail.com>
2019-08-22 21:33:03 +0200

    add hard-coded octave and halfstep markers to columns

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -8,6 +8,41 @@ export const Column: FC = () => {
   return (
     <div className="column">
       <div className="chord-slider" />
+      <div className="octave-markers">
+        <div className="octave-5" />
+        <div className="octave-4" />
+        <div className="octave-3" />
+      </div>
+      <div className="halfstep-markers">
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+        <div className="halfstep" />
+      </div>
       <Chord />
     </div>
   );
